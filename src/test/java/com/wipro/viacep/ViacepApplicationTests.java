a50dabd45304482135aab2dbfb3cdb5eb2dbcb17
rrresck <rrresck@gmail.com>
2023-03-31 03:33:13 -0300

    first commit

---
+++ src/test/java/com/wipro/viacep/ViacepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.viacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViacepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
