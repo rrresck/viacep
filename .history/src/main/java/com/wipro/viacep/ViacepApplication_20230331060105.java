c522d93bd0c3478f238859ad53700341e8ddaac5
rrresck <rrresck@gmail.com>
2023-03-31 03:40:57 -0300

    segundo commit

--- .history/src/main/java/com/wipro/viacep/ViacepApplication_20230331060105.java
+++ .history/src/main/java/com/wipro/viacep/ViacepApplication_20230331060105.java
@@ -2,12 +2,7 @@ package com.wipro.viacep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@EnableSwagger2
-@EnableFeignClients
 @SpringBootApplication
 public class ViacepApplication {
 
