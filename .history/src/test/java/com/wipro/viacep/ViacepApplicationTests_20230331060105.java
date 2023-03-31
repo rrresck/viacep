bb17ca626cdc9cf7311b64bec650c6f029be5460
rrresck <rrresck@gmail.com>
2023-03-31 03:50:20 -0300

    commit 2

--- .history/src/test/java/com/wipro/viacep/ViacepApplicationTests_20230331060105.java
+++ .history/src/test/java/com/wipro/viacep/ViacepApplicationTests_20230331060105.java
@@ -2,11 +2,9 @@ package com.wipro.viacep;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @SpringBootTest
-class FreteViacepWiproApplicationTests {
+class ViacepApplicationTests {
 
 	@Test
 	void contextLoads() {
